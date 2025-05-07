65a9789730d326f5f286215f47e3866c0f559230
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-05-08 00:34:34 +0530

    Update: Added Comment Feature

--- blogapp/app/Config/Routes.php
+++ blogapp/app/Config/Routes.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+// Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
 /*
@@ -14,6 +15,11 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
+// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
+// where controller filters or CSRF protection are bypassed.
+// If you don't want to define all routes, please use the Auto Routing (Improved).
+// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
+// $routes->setAutoRoute(false);
 
 /*
  * --------------------------------------------------------------------
@@ -21,9 +27,18 @@ $routes->set404Override();
  * --------------------------------------------------------------------
  */
 
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
 $routes->get('/', 'HomeController::index');
 $routes->get('post/(:num)', 'HomeController::show/$1');
 
+// Blog routes
+$routes->get('posts', 'PostController::index');
+$routes->get('posts/create', 'PostController::create');
+$routes->post('posts/store', 'PostController::store');
+$routes->get('posts/edit/(:num)', 'PostController::edit/$1');
+$routes->post('posts/update/(:num)', 'PostController::update/$1');
+$routes->post('posts/delete/(:num)', 'PostController::delete/$1');
 
 // Blog Post Management Routes
 $routes->group('admin', function($routes) {
@@ -35,6 +50,19 @@ $routes->group('admin', function($routes) {
     $routes->post('blog-posts/delete/(:num)', 'Admin\BlogPostController::delete/$1');
 });
 
+/*
+ * --------------------------------------------------------------------
+ * Additional Routing
+ * --------------------------------------------------------------------
+ *
+ * There will often be times that you need additional routing and you
+ * need it to be able to override any defaults in this file. Environment
+ * based routes is one such time. require() additional route files here
+ * to make that happen.
+ *
+ * You will have access to the $routes object within that file without
+ * needing to reload it.
+ */
 if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
     require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
